2c7f62099fbcf906488eecba8c61e938ad157af3
alimranhossain <alimranhossain44@gmail.com>
2022-10-25 12:47:31 +0600

    header part design

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Header from './Pages/Shared/Header/Header';
 function App() {
   return (
     <div className="App">
-     <h2>WelCome to PI Care</h2>
-     <Header></Header>
+    <Header></Header>
     </div>
   );
 }
