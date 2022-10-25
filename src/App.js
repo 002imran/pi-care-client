61ade5c09e52e07ecc7d146ea2f48f9a758b7a99
alimranhossain <alimranhossain44@gmail.com>
2022-10-25 11:40:30 +0600

    folder structure and file setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
      <h2>WelCome to PI Care</h2>
+     <Header></Header>
     </div>
   );
 }
